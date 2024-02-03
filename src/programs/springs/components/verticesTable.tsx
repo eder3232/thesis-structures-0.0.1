b810b610fd6608e8839203a8461f8cb4f931b043
eder3232 <eldebar.dk@gmail.com>
2024-02-03 08:23:52 -0500

    Add new row and delete row functionality to VerticesTable component

--- src/programs/springs/components/verticesTable.tsx
+++ src/programs/springs/components/verticesTable.tsx
@@ -2,7 +2,9 @@ import { useAtom } from 'jotai'
 import { atomGetAreDofDefinedByUser } from '../store/areDofDefinedByUser'
 import {
   atomGetVertices,
+  atomSetVerticesAddNewRow,
   atomSetVerticesBoolean,
+  atomSetVerticesDeleteRow,
   atomSetVerticesNumber,
   atomSetVerticesString,
 } from '../store/vertices'
@@ -27,6 +29,8 @@ const VerticesTable = () => {
   const [, setVerticesString] = useAtom(atomSetVerticesString)
   const [, setVerticesNumber] = useAtom(atomSetVerticesNumber)
   const [, setVerticesBoolean] = useAtom(atomSetVerticesBoolean)
+  const [, setVerticesAddNewRow] = useAtom(atomSetVerticesAddNewRow)
+  const [, setVerticesDeleteRow] = useAtom(atomSetVerticesDeleteRow)
 
   return (
     <div className="max-w-full overflow-x-auto overflow-y-visible md:max-w-min">
@@ -40,9 +44,14 @@ const VerticesTable = () => {
                 </Button>
               </TableHead>
 
-              <TableHead className="text-primary px-0">
-                <Button size="icon" variant="outline">
-                  <MinusCircle className="m-autp" />
+              <TableHead className="px-0">
+                <Button
+                  size="icon"
+                  variant="outline"
+                  className="disabled:text-muted-foreground"
+                  disabled
+                >
+                  <MinusCircle className="m-auto" />
                 </Button>
               </TableHead>
 
@@ -76,6 +85,7 @@ const VerticesTable = () => {
                     size="icon"
                     variant="outline"
                     className="text-primary"
+                    onClick={() => setVerticesAddNewRow(index)}
                   >
                     <PlusCircle className="m-auto" />
                   </Button>
@@ -87,7 +97,10 @@ const VerticesTable = () => {
                     variant="outline"
                     className="text-primary"
                   >
-                    <MinusCircle className="m-auto" />
+                    <MinusCircle
+                      className="m-auto"
+                      onClick={() => setVerticesDeleteRow(index)}
+                    />
                   </Button>
                 </TableCell>
 
--- src/programs/springs/store/vertices.tsx
+++ src/programs/springs/store/vertices.tsx
@@ -4,6 +4,7 @@ import { produce } from 'immer'
 import { v4 as uuidv4 } from 'uuid'
 import { IInputReactVertices } from '../interfaces/vertices'
 import { initialVerticesData } from '../data/data1'
+import { get } from 'http'
 
 const atomVertices = atom<IInputReactVertices[]>(initialVerticesData)
 
@@ -70,6 +71,37 @@ export const atomSetVerticesBoolean = atom(
       atomVertices,
       produce((draft) => {
         draft[index][field] = value
+      })
+    )
+  }
+)
+
+export const atomSetVerticesAddNewRow = atom(
+  null,
+  (_get, set, index: number) => {
+    set(
+      atomVertices,
+      produce((draft) => {
+        draft.splice(index + 1, 0, {
+          id: uuidv4(),
+          name: `v${draft.length + 1}`,
+          force: 0,
+          displacement: 0,
+          isRestricted: false,
+          userDOF: draft.length + 1,
+        })
+      })
+    )
+  }
+)
+
+export const atomSetVerticesDeleteRow = atom(
+  null,
+  (_get, set, index: number) => {
+    set(
+      atomVertices,
+      produce((draft) => {
+        draft.splice(index, 1)
       })
     )
   }
