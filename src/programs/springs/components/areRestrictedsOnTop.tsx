2cd795558f931c59c047485c622d3741ee8c399e
eder3232 <eldebar.dk@gmail.com>
2024-02-04 08:08:16 -0500

    Agregada funcionalidad de casilla de verificación para los grados de libertad restringidos en la parte superior

--- src/programs/springs/components/areRestrictedsOnTop.tsx
+++ src/programs/springs/components/areRestrictedsOnTop.tsx
@@ -1,11 +1,25 @@
 import { Checkbox } from '@/components/ui/checkbox'
+import { useAtom } from 'jotai'
+import {
+  atomGetAreRestrictedOnTop,
+  atomSetSwitchAreRestrictedOnTop,
+} from '../store/areRestrictedsOnTop'
 
 const AreRestrictedOnTop = () => {
+  const [areRestrictedOnTop] = useAtom(atomGetAreRestrictedOnTop)
+
+  const [, setSwitchAreRestrictedOnTop] = useAtom(
+    atomSetSwitchAreRestrictedOnTop
+  )
+
   return (
     <div className="flex items-center gap-4">
       <span>¿Los grados de libertad restringidos van arriba?</span>
 
-      <Checkbox />
+      <Checkbox
+        checked={areRestrictedOnTop}
+        onCheckedChange={() => setSwitchAreRestrictedOnTop()}
+      />
     </div>
   )
 }
