7ba492debd026b8ec3baecf8142687c3c5f31959
eder3232 <eldebar.dk@gmail.com>
2024-02-15 11:46:00 -0500

    Add components areRestrictedOnTop and AreDofDefinedByUser

---
+++ src/programs/armor3d/store/areDofDefinedByUser.tsx
@@ -0,0 +1,20 @@
+import { atom } from 'jotai'
+import { atomSetVerticesResetDofs } from './vertices'
+import { atomGetAreRestrictedOnTop } from './areRestrictedsOnTop'
+
+const atomAreDofDefinedByUser = atom(true)
+
+export const atomGetAreDofDefinedByUser = atom((get) =>
+  get(atomAreDofDefinedByUser)
+)
+
+export const atomSetSwitchAreDofDefinedByUser = atom(null, (_get, set) => {
+  const currentValue = _get(atomAreDofDefinedByUser)
+  if (currentValue) {
+    set(atomSetVerticesResetDofs, {
+      areRestrictedOnTop: _get(atomGetAreRestrictedOnTop),
+    })
+  }
+
+  set(atomAreDofDefinedByUser, !currentValue)
+})
