14a46834f704259b6a52ba46bfced3c6e78f3b2a
eder3232 <eldebar.dk@gmail.com>
2024-02-22 07:39:00 -0500

    Add db config

--- src/db/lib/db.ts
+++ src/db/lib/db.ts
@@ -1,24 +1,6 @@
-import './config'
-import { drizzle } from 'drizzle-orm/vercel-postgres'
 import { sql } from '@vercel/postgres'
-import { users } from '../schema'
+import { drizzle } from 'drizzle-orm/vercel-postgres'
 import * as schema from '../schema'
+import './config'
 
 export const db = drizzle(sql, { schema })
-
-export const getUsers = async () => {
-  const selectResult = await db.select().from(users)
-  console.log('Results', selectResult)
-  return selectResult
-}
-
-export type NewUser = typeof users.$inferInsert
-
-export const insertUser = async (user: NewUser) => {
-  return db.insert(users).values(user).returning()
-}
-
-export const getUsers2 = async () => {
-  const result = await db.query.users.findMany()
-  return result
-}
