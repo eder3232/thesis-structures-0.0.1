3f24799cd6294d4f4b66990bf40bb5d35b8e307c
eder3232 <eldebar.dk@gmail.com>
2024-02-25 06:39:04 -0500

    Add Roller suport for graphic2D

---
+++ src/programs/armor2d/components/graph/rollerSuport.tsx
@@ -0,0 +1,46 @@
+import { Cone, Sphere } from '@react-three/drei'
+
+interface Props {
+  applicationPoint: [number, number, number]
+  directorCosines: [number, number, number]
+  sphereSize: number
+  size: number
+}
+const RollerSuport = ({ size, sphereSize, applicationPoint }: Props) => {
+  const coneWidth = size
+  const coneHeight = size
+  return (
+    <group position={applicationPoint}>
+      <Cone
+        args={[coneWidth, coneHeight, 4, 1]}
+        position={[0, -coneHeight / 2 - sphereSize, 0]}
+        rotation={[0, Math.PI / 4, 0]}
+      >
+        <meshBasicMaterial color="blue" />
+      </Cone>
+
+      <Sphere
+        args={[size / 4]}
+        position={[-size / 2, -coneHeight - size / 4 - sphereSize, 0]}
+      >
+        <meshBasicMaterial color="blue" />
+      </Sphere>
+
+      <Sphere
+        args={[size / 4]}
+        position={[0, -coneHeight - size / 4 - sphereSize, 0]}
+      >
+        <meshBasicMaterial color="blue" />
+      </Sphere>
+
+      <Sphere
+        args={[size / 4]}
+        position={[size / 2, -coneHeight - size / 4 - sphereSize, 0]}
+      >
+        <meshBasicMaterial color="blue" />
+      </Sphere>
+    </group>
+  )
+}
+
+export default RollerSuport
