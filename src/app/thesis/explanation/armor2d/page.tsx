bbe9c8dda864499e5ea4654275d424417bd93fa6
eder3232 <eldebar.dk@gmail.com>
2024-06-07 09:34:06 -0500

    Add armor2d explanation page

---
+++ src/app/thesis/explanation/armor2d/page.tsx
@@ -0,0 +1,11 @@
+import ExplanationArmor2D from '@/content/explanation/armor2d'
+
+const Page = () => {
+  return (
+    <div>
+      <ExplanationArmor2D />
+    </div>
+  )
+}
+
+export default Page
